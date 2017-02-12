4618a00dccfd8bb50074239d51cab6bf7d8c2cc8
Betty <smckoy@gmail.com>
2017-02-12 21:46:17 +1300

    simplify recorder

--- scripts/sagas/action-recorder.tsx
+++ scripts/sagas/action-recorder.tsx
@@ -1,23 +1,20 @@
 import { delay, takeEvery, takeLatest, eventChannel, END } from 'redux-saga'
 import { race, fork, take, call, put, select } from 'redux-saga/effects'
-import inputChannel from '../utils/input-channel.tsx';
 import Action from '../utils/action.tsx';
 
-
-export default function* actionRecorder(chan, recording : Array<Action>) {
-  const inputChan = yield call(inputChannel);
+export default function* actionRecorder(chan, recording: Array<Action>) {
   let timestamp: number = 0;
 
-  while (true) {
-    let event = yield race({
-      input: take(inputChan),
-      msg: take(chan, 'GAMELOOP_TICK')
-    });
+  var emitter = function (event) {
+    console.log(event, timestamp);
+    recording.push({ ...event, timestamp: timestamp });
+  };
 
-    if (event.msg) {
-      timestamp = event.msg.payload.timestamp;
-    } else {      
-      recording.push({ ...event.input, timestamp: timestamp });      
-    }
+  window.addEventListener('keypress', e => emitter({ type: 'KEYPRESS', payload: { key: e.key } }));
+  window.addEventListener('click', e => emitter({ type: 'CLICK', payload: { x: e.clientX, y: e.clientY, button: e.which } }));
+
+  while (true) {
+    var event = yield take(chan, 'GAMELOOP_TICK');
+    timestamp = event.payload.timestamp;
   }
 }
