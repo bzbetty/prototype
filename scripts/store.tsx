499eeb9138608710dc8ba2b9ddfa8603c4be0563
Betty <smckoy@gmail.com>
2017-02-13 22:34:54 +1300

    remove copy pasta cruft

--- scripts/store.tsx
+++ scripts/store.tsx
@@ -5,13 +5,6 @@ import createSagaMiddleware from 'redux-saga'
 import rootReducer from './reducers/index.tsx';
 import rootSaga from './sagas/index.tsx';
 
-// import createSignalrMiddleware from './middleware/signalrMiddleware.jsx';
-// import tokenInjectorMiddleware from './middleware/tokenInjectorMiddleware.jsx';
-// import cookieMiddleware from './middleware/cookieMiddleware.jsx';
-// import retryMiddleware from './middleware/retryMiddleware.jsx';
-// import clearOnNavigate from './middleware/clearOnNavigateMiddleware.jsx';
-// clearOnNavigate
-
 const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
